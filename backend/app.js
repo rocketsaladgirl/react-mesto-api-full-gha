20aad90dabe7080401883c10d14d9439786fbcb7
rocketsaladgirl <114432448+rocketsaladgirl@users.noreply.github.com>
2023-05-26 23:38:10 +0300

    изменяем адрес подключения к базе данных

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,7 @@ const auth = require('./middlewares/auth');
 const handleError = require('./middlewares/handelError');
 
 const {
-  MONGO_URL = 'mongodb://localhost:27017/mestodb',
+  MONGO_URL = 'mongodb://127.0.0.1:27017/mestodb',
   PORT = 3000,
 } = process.env;
 
